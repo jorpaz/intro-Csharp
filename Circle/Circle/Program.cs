519c5f6b1cb97628744f4c8184dbf107d35a9c49
jorpaz <giovanny.paz@outlook.com>
2023-03-06 22:40:56 -0600

    Primeros pasos en C#, solo son algunos ejercicios introductorios

---
+++ Circle/Circle/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+var radio = 0d;
+var resultado = 0d;
+
+Console.WriteLine("Bienvenido, calculemos circulos!");
+//Pi * radio*radio
+
+Console.WriteLine("Ingrese el valor del radio");
+
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultado = Math.PI * radio * radio;
+
+Console.WriteLine("El area de tu circulo es de : " + resultado);
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//Programa que calcula el área de un rectángulo
+
+double ladoA; //Declaracion de variable Explicita
+var ladoB = 1.1; //Implicita
+var resultado = 1.1;
+
+Console.WriteLine("Calcula el área de un rectángulo!!");
+Console.WriteLine("Ingrese el valor del lado A");
+ladoA =  Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+Console.WriteLine("El lado A es: " + ladoA + " El lado B es: " + ladoB + " El resultado es: " + resultado);
+
