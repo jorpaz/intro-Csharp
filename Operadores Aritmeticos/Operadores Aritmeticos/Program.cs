8a3e94cc73ba6c8dbad5dced8c9dbf2d34ff4fee
jorpaz <giovanny.paz@outlook.com>
2023-03-06 23:00:49 -0600

    operadores aritmeticos y operadores logicos

---
+++ Operadores Aritmeticos/Operadores Aritmeticos/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Otro programa que calcule el area de un rectangulo
+
+var ladoA = 0d; //Declaracion de variables
+var ladoB = 0d;
+var resultado = 0d;
+
+Console.WriteLine("Calcula el area de un rectangulo!!");
+Console.WriteLine("Ingrese el valor del lado A");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+//ladoA++; //le suma + 1, operador de incremento => ladoA = ladoA + 1;
+//ladoA--; //le resta -1, operador de decremento;
+//Multiplicacion => ladoA * ladoB;
+//Division =>  ladoA / ladoB;
+// El residuo de una division => ladoA % ladoB
+
+resultado = ladoA * ladoB;
+Console.WriteLine("El lado A es: " + ladoA + " El lado B es: " + ladoB + " El resultado es: " + resultado);
